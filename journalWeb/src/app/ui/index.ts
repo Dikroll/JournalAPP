9695ed3bc799aa94d5ab7c50b7cd2367b4b07c99
Dikroll <bushmelew07@gmail.com>
2026-04-10 16:24:07 +0300

    del: app ui

--- journalWeb/src/app/ui/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @fileoverview App UI components - компоненты приложения
- *
- * Экспортирует:
- * - ThemeToggleButton - кнопка переключения темы
- */
-
-export { ThemeToggleButton } from './ThemeToggleButton'
